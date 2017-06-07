490eb1335d1200cafc81966541ba42e5152d3381
Daksh Shah <dakshshah96@gmail.com>
2017-06-07 16:08:24 +0300

    Starter files and environment variables

---
+++ start.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Make sure we are running node 7.6+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if (major < 7 || (major === 7 && minor <= 5)) {
+  console.log('Dang! You\'re running an old version of Node.js. Please install Node.js 7.6+ and be back here.');
+  process.exit();
+}
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle an bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+// READY?! Let's go!
+
+
+// Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
