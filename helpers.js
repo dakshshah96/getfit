5ae29a1f9bd460d32e73981ce6ffe1ff66eb174b
Daksh Shah <dakshshah96@gmail.com>
2017-06-08 14:19:11 +0300

    Cleanup navigation
    * Change logo to GetFit
    * Keep only Profiles tab
    * Change search placeholder text

--- helpers.js
+++ helpers.js
@@ -21,9 +21,5 @@ exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 exports.siteName = `Now That's Delicious!`;
 
 exports.menu = [
-  { slug: '/stores', title: 'Stores', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/map', title: 'Map', icon: 'map', },
+  { slug: '/profiles', title: 'Profiles', icon: 'tag', }
 ];
