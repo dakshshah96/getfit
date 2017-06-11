3592d6076e568ed394874a9d55203e0a8b5d65d8
Daksh Shah <dakshshah96@gmail.com>
2017-06-11 16:50:43 +0300

    Styling and JS improvements
    * Remove Google Maps scripts
    * Fix profile card description styles
    * Change body font to Roboto
    * Correct individual profile page styling
    * Increase search results font size

--- helpers.js
+++ helpers.js
@@ -11,9 +11,6 @@ exports.moment = require('moment');
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-// Making a static map is really long - this is a handy helper function to make one
-exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
-
 // inserting an SVG
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
